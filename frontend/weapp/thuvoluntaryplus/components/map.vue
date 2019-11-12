1332c8fe70cd4b7a0f2ceb3c62db9d73ce9b3ad4
金昕祺 <jinxq16@mails.tsinghua.edu.cn>
2019-11-12 17:33:53 +0800

    first commit

---
+++ frontend/weapp/thuvoluntaryplus/components/map.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="cu-card">
+		<view class="cu-item">
+			<!-- 在map标签绑定经纬度和标记点数组-->
+			<map id="maps" :latitude="targetLocation.latitude" :longitude="targetLocation.longitude" :markers="targetLocation.markers" scale="80"></map>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+	    mapState,  
+	    mapMutations
+	} from 'vuex'
+	export default{
+		name: "Map",
+		props: {
+			targetLocation: {
+				type: Object,
+				required: true
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
