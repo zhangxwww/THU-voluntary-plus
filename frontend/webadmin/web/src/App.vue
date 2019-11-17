8f64a909ef225efdb52478e5a53354f00e91b5e3
agil27 <wang-yb16@mails.tsinghua.edu.cn>
2019-11-18 05:52:10 +0800

    Navbar

--- frontend/webadmin/web/src/App.vue
+++ frontend/webadmin/web/src/App.vue
@@ -1,8 +1,38 @@
 <template>
   <div id="app">
-    <router-view/>
+    <el-container>
+      <el-header>
+        <nav-bar></nav-bar>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>
+          <router-view />
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
+
+<script>
+import NavBar from '@/layout/NavBar'
+export default{
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
 
 <style>
 </style>
---
+++ frontend/webadmin/web/src/layout/SideBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="showLogo">
+    <sidebar-logo collapse=false />
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        :collapse="false"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :unique-opened="false"
+        :active-text-color="variables.menuActiveText"
+        :collapse-transition="false"
+        mode="vertical"
+      >
+        <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+import SidebarLogo from '@/components/SidebarLogo'
+import SidebarItem from '@/components/SidebarItem'
+import variables from '@/styles/variables.scss'
+
+export default {
+  components: { SidebarItem, SidebarLogo },
+  computed: {
+    routes() {
+      return this.$router.options.routes
+    },
+    variables() {
+      return variables
+    }
+  }
+}
+</script>
