620bd7af531ae6ad7495bd30258d854ae516875a
zhangxwww <631133544@qq.com>
2019-11-21 10:31:42 +0800

    finish add new activity

--- frontend/webadmin/web/src/script/send.js
+++ frontend/webadmin/web/src/script/send.js
@@ -1,5 +1,27 @@
-export function addNewActivity(form, callback) {
-    alert('1234')
+import axios from 'axios'
+
+export function addNewActivity(form, success, fail) {
     alert(form.name)
-    callback()
+    alert(form.region)
+    alert(form.date1)
+    alert(form.date2)
+    alert(form.tag)
+    alert(form.desc)
+
+    axios.post('/api/activity/new', {
+        name: form.name,
+        region: form.region,
+        date1: form.date1,
+        date2: form.date2,
+        tag: form.tag,
+        desc: form.desc
+    }).then(function (res) {
+        if (res.state === 'success') {
+            success()
+        } else {
+            fail()
+        }
+    }).catch(function () {
+        fail()
+    })
 }
