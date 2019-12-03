367848ef4a0740fa4480a70c0840bc4c73c7d56b
agil27 <wang-yb16@mails.tsinghua.edu.cn>
2019-12-03 20:26:40 +0800

    announce

--- frontend/webadmin/web/src/views/ConfigActivity.vue
+++ frontend/webadmin/web/src/views/ConfigActivity.vue
@@ -1,16 +1,18 @@
 <template>
     <el-tabs v-model="activeName" type="border-card" @tab-click="handleClick">
         <el-tab-pane label="工时分配" name="alloc"><allocation /></el-tab-pane>
-        <el-tab-pane label="发布通知" name="announce">发布通知</el-tab-pane>
+        <el-tab-pane label="发布通知" name="announce"><announce /></el-tab-pane>
         <el-tab-pane label="查看反馈" name="feedback">查看反馈</el-tab-pane>
     </el-tabs>
 </template>
 
 <script>
 import Allocation from './Allocation'
+import Announce from "./Announce";
 export default {
     components: {
-        Allocation
+        Allocation,
+        Announce
     },
     data() {
         return {
