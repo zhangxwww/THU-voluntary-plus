8465ec636be7f2f0490169f53a1fc4ac42591295
agil27 <wang-yb16@mails.tsinghua.edu.cn>
2019-12-15 12:13:36 +0800

    edit activity

--- frontend/webadmin/web/src/views/ActivityCenter.vue
+++ frontend/webadmin/web/src/views/ActivityCenter.vue
@@ -85,6 +85,108 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-dialog :title="编辑活动" :visible-sync="editVisible">
+      <el-form :model="editForm"
+               :rules="rules"
+               ref="editForm"
+               label-width="100px"
+               class="demo-editForm">
+        <el-form-item label="活动名称"
+                      prop="name">
+          <el-input v-model="editForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="所在省份"
+                      prop="city">
+          <el-select v-model="editForm.city"
+                     placeholder="请选择所在省份">
+            <el-option v-for="pro in provinces"
+                       :label="pro.label"
+                       :value="pro.value"
+                       :key="pro.id"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="具体地点"
+                      prop="location">
+          <el-input v-model="editForm.location"></el-input>
+        </el-form-item>
+        <el-form-item label="活动人数"
+                      prop="totalNum">
+          <el-input-number v-model="editForm.totalNum"
+                           :min="1"
+                           :max="50"
+                           label="活动人数"></el-input-number>
+        </el-form-item>
+        <el-form-item label="开始时间"
+                      required>
+          <el-row :gutter="20">
+            <el-col :span="11">
+              <el-form-item prop="startdate">
+                <el-date-picker type="date"
+                                placeholder="选择日期"
+                                v-model="editForm.startdate"
+                                style="width: 100%;"></el-date-picker>
+              </el-form-item>
+            </el-col>
+            <el-col :span="11">
+              <el-form-item prop="starttime">
+                <el-time-picker placeholder="选择时间"
+                                v-model="editForm.starttime"
+                                style="width: 100%;"></el-time-picker>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item label="结束时间"
+                      required>
+          <el-row :gutter="20">
+            <el-col :span="11">
+              <el-form-item prop="enddate">
+                <el-date-picker type="date"
+                                placeholder="选择日期"
+                                v-model="editForm.enddate"
+                                style="width: 100%;"></el-date-picker>
+              </el-form-item>
+            </el-col>
+            <el-col :span="11">
+              <el-form-item prop="endtime">
+                <el-time-picker placeholder="选择时间"
+                                v-model="editForm.endtime"
+                                style="width: 100%;"></el-time-picker>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item label="活动标签"
+                      prop="tag">
+          <el-input class="input-new-tag"
+                    v-model="inputValue"
+                    v-if="inputVisible"
+                    size="small"
+                    @keyup.enter.native="handleInputConfirm"
+                    @blur="handleInputConfirm">
+          </el-input>
+          <el-button v-else
+                     plain
+                     type="primary"
+                     class="button-new-tag"
+                     size="small"
+                     @click="showInput">
+            {{ editForm.tag }}
+          </el-button>
+        </el-form-item>
+        <el-form-item label="活动简介"
+                      prop="desc">
+          <el-input type="textarea"
+                    v-model="editForm.desc"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="success"
+                     @click="submitEdit">确认
+          </el-button>
+          <el-button type="info" @click="cancelEdit">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
     <div class="pagination">
       <el-pagination background
                      @size-change="handleSizeChange"
@@ -120,7 +222,81 @@ export default {
       listLoading: false,
       search: '',
       pagesize: 3,
-      list: []
+      list: [],
+      inputVisible: false,
+      inputValue: '',
+      editVisible: false,
+      currentEditRow: null,
+      provinces: [
+        {
+          id: 0,
+          label: '北京',
+          value: '北京'
+        },
+        {
+          id: 1,
+          label: '上海',
+          value: '上海'
+        }
+      ],
+      ruleForm: {
+        name: '',
+        region: '',
+        totalNum: 1,
+        startdate: '',
+        starttime: '',
+        enddate: '',
+        endtime: '',
+        delivery: false,
+        tag: '',
+        desc: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
+        ],
+        city: [
+          { required: true, message: '请选择所在省份', trigger: 'change' }
+        ],
+        location: [
+          { required: true, message: '请输入具体地点', trigger: 'change' }
+        ],
+        totalNum: [{ required: true }],
+        startdate: [
+          {
+            type: 'date',
+            required: true,
+            message: '请选择日期',
+            trigger: 'change'
+          }
+        ],
+        starttime: [
+          {
+            type: 'date',
+            required: true,
+            message: '请选择时间',
+            trigger: 'change'
+          }
+        ],
+        enddate: [
+          {
+            type: 'date',
+            required: true,
+            message: '请选择日期',
+            trigger: 'change'
+          }
+        ],
+        endtime: [
+          {
+            type: 'date',
+            required: true,
+            message: '请选择时间',
+            trigger: 'change'
+          }
+        ],
+        desc: [{ required: true, message: '请填写活动简介诶', trigger: 'blur' }]
+      }
     }
   },
   computed: {
@@ -160,8 +336,10 @@ export default {
     handleDelete (index, row) {
       alert(index, row)
     },
+    // eslint-disable-next-line no-unused-vars
     handleEdit (index, row) {
-      alert(index, row)
+      this.editVisible = true
+      this.currentEditRow = row
     },
     handleDetail (index, row) {
       alert(row.id)
@@ -184,7 +362,22 @@ export default {
     handleCurrentChange (val) {
       this.page = val
       this.getList()
-    }
+    },
+    submitEdit () {
+      this.editVisible = false
+    },
+    cancelEdit() {
+      this.editVisible = false
+    },
+    handleClose (tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1)
+    },
+    showInput () {
+      this.inputVisible = true
+      this.$nextTick(function () {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
   }
 }
 </script>
