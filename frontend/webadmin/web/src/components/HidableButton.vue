4661f1a135cf180243d865cd9e9dc96bf0333cf7
zhangxwww <631133544@qq.com>
2019-12-16 20:48:32 +0800

    read feedback in web

--- frontend/webadmin/web/src/components/HidableButton.vue
+++ frontend/webadmin/web/src/components/HidableButton.vue
@@ -1,73 +1,73 @@
 <template>
-    <el-button
-            :icon="icon"
-            :size="size"
-            :type="type"
-            :circle="isCircle"
-            :round="isRound"
-            @mouseenter.native="onHover"
-            @mouseleave.native="onLeave"
-            @click="handleCheck(scope.$index, scope.row)">{{ buttontext }}
-    </el-button>
+  <el-button :icon="icon"
+             :size="size"
+             :type="type"
+             :circle="isCircle"
+             :round="isRound"
+             @mouseenter.native="onHover"
+             @mouseleave.native="onLeave">{{ buttontext }}
+  </el-button>
 </template>
 
 <script>
-    export default {
-        props: {
-            icon: {
-                type: String,
-                default: ''
-            },
-            size: {
-                type: String,
-                default: 'mini'
-            },
-            text: {
-                type: String,
-                default: ''
-            },
-            type: {
-                type: String,
-                default: "primary"
-            },
-            shape: {
-                type: String,
-                default: ""
-            }
-        },
-        name: "HidableButton",
-        data() {
-            return {
-                hover: false
-            }
-        },
-        computed: {
-            buttontext: function () {
-                window.console.log(this.hover);
-                if (this.hover) {
-                    return this.text
-                } else {
-                    return ''
-                }
-            },
-            isRound: function () {
-                return this.shape === 'round'
-            },
-            isCircle: function () {
-                return this.shape === 'circle'
-            }
-        },
-        methods: {
-            onHover: function () {
-                this.hover = true
-            },
-            onLeave: function () {
-                this.hover = false
-            }
-        }
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: String,
+      default: 'mini'
+    },
+    text: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: "primary"
+    },
+    shape: {
+      type: String,
+      default: ""
+    },
+    clickHandler: {
+      default: null
     }
+  },
+  name: "HidableButton",
+  data () {
+    return {
+      hover: false
+    }
+  },
+  computed: {
+    buttontext: function () {
+      window.console.log(this.hover);
+      if (this.hover) {
+        return this.text
+      } else {
+        return ''
+      }
+    },
+    isRound: function () {
+      return this.shape === 'round'
+    },
+    isCircle: function () {
+      return this.shape === 'circle'
+    }
+  },
+  methods: {
+    onHover: function () {
+      this.hover = true
+    },
+    onLeave: function () {
+      this.hover = false
+    }
+  }
+}
 </script>
 
 <style scoped>
-
 </style>
